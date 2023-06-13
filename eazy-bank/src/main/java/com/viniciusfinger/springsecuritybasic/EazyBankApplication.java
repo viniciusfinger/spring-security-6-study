a61e5d70e9a95496899628d508685ba0ab053269
ViniciusFinger <fingervinicius@icloud.com>
2023-06-13 12:49:58 -0300

    eazy bank application

---
+++ eazy-bank/src/main/java/com/viniciusfinger/springsecuritybasic/EazyBankApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusfinger.springsecuritybasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyBankApplication.class, args);
+	}
+
+}
---
+++ eazy-bank/src/test/java/com/viniciusfinger/springsecuritybasic/EazyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusfinger.springsecuritybasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
