26e156f60a12db129b8b7817c784628ba6c35cee
ViniciusFinger <fingervinicius@icloud.com>
2023-06-14 18:22:37 -0300

    creating in memory users

--- eazy-bank/src/main/java/com/viniciusfinger/eazybank/config/SecurityConfig.java
+++ eazy-bank/src/main/java/com/viniciusfinger/eazybank/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package com.viniciusfinger.eazybank.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -25,5 +28,22 @@ public class SecurityConfig {
                 .formLogin(withDefaults())
                 .httpBasic(withDefaults())
                 .build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManger(){
+        UserDetails userAdmin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .authorities("admin")
+                .build();
+
+        UserDetails userFinger = User.withDefaultPasswordEncoder()
+                .username("finger")
+                .password("123456")
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(userAdmin, userFinger);
     }
 }
