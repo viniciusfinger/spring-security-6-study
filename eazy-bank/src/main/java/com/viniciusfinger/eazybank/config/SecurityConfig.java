8c64298c04f29aac6ea0c9021fbd10a4b9aad50f
Vinicius Finger <fingervinicius@icloud.com>
2023-08-01 15:02:17 -0300

    configuring cross origin resource sharing

--- eazy-bank/src/main/java/com/viniciusfinger/eazybank/config/SecurityConfig.java
+++ eazy-bank/src/main/java/com/viniciusfinger/eazybank/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.viniciusfinger.eazybank.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,25 +10,53 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+
+import java.util.Arrays;
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("*"));
+        config.setAllowCredentials(true);
+        config.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        config.setMaxAge(3600L); //24h
+        return source;
+    }
+
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        return http.authorizeHttpRequests(requests ->
+        http.cors().configurationSource(request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+            config.setAllowedMethods(Arrays.asList("*"));
+            config.setAllowCredentials(true);
+            config.setAllowedHeaders(Arrays.asList("*"));
+            config.setMaxAge(3600L); //24h
+            return config;
+        }).and()
+                .csrf().disable()
+                .authorizeHttpRequests(requests ->
                 requests
                         .requestMatchers("/notices/**", "/contacts/**", "/auth/**").permitAll()
                         .requestMatchers("/accounts/**", "/balances/**", "/loans/**", "/cards/**", "/users/**").authenticated()
                 )
                 .formLogin(withDefaults())
-                .httpBasic(withDefaults())
-                .csrf().disable()
-                .build();
+                .httpBasic(withDefaults());
+
+        return http.build();
     }
 
     //Spring standard JDBC user details
