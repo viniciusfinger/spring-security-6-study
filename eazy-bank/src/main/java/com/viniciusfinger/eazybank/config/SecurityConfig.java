682cada75d3eda7702538b832fca0bd622339d0e
Vinicius Finger <fingervinicius@icloud.com>
2023-08-01 15:13:20 -0300

    improving CORS configuration

--- eazy-bank/src/main/java/com/viniciusfinger/eazybank/config/SecurityConfig.java
+++ eazy-bank/src/main/java/com/viniciusfinger/eazybank/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,31 +24,18 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class SecurityConfig {
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("*"));
-        config.setAllowedMethods(Arrays.asList("*"));
-        config.setAllowCredentials(true);
-        config.setAllowedHeaders(Arrays.asList("*"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        config.setMaxAge(3600L); //24h
-        return source;
-    }
-
-
-    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(request -> {
-            CorsConfiguration config = new CorsConfiguration();
-            config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-            config.setAllowedMethods(Arrays.asList("*"));
-            config.setAllowCredentials(true);
-            config.setAllowedHeaders(Arrays.asList("*"));
-            config.setMaxAge(3600L); //24h
-            return config;
-        }).and()
-                .csrf().disable()
+        http
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+                    config.setAllowedMethods(Arrays.asList("*"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(Arrays.asList("*"));
+                    config.setMaxAge(3600L); //24h
+                    return config;
+                }))
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests ->
                 requests
                         .requestMatchers("/notices/**", "/contacts/**", "/auth/**").permitAll()
