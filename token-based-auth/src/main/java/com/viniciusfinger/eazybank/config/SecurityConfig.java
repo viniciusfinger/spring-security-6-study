e14c5ecf72828de0ce1a548776343046b309929d
Vinicius Finger <fingervinicius@icloud.com>
2023-08-21 14:11:28 -0300

    pre authorize

--- token-based-auth/src/main/java/com/viniciusfinger/eazybank/config/SecurityConfig.java
+++ token-based-auth/src/main/java/com/viniciusfinger/eazybank/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.viniciusfinger.eazybank.config;
 import com.viniciusfinger.eazybank.filter.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +19,7 @@ import java.util.Arrays;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
--- token-based-auth/src/main/java/com/viniciusfinger/eazybank/repository/AccountTransactionRepository.java
+++ token-based-auth/src/main/java/com/viniciusfinger/eazybank/repository/AccountTransactionRepository.java
@@ -2,6 +2,7 @@ package com.viniciusfinger.eazybank.repository;
 
 import com.viniciusfinger.eazybank.model.AccountTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 @Repository
 public interface AccountTransactionRepository extends JpaRepository<AccountTransaction, Long> {
 
+    @PreAuthorize("hasRole('USER')")
     List<AccountTransaction> findByCustomerIdOrderByTransactionDtDesc(Integer customerId);
 
 }
